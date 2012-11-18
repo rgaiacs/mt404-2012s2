7a50eee8117c98a96886e8107450161d359d51f5
Raniere Silva <r.gaia.cs@gmail.com>
2012-11-18 20:27:32 -0200

    Init Proj07.

---
+++ proj07/src/mt404_p07.f
@@ -0,0 +1,18 @@
+c Copyright (C) 2012 Raniere Silva <r.gaia.cs@gmail.com>
+c 
+c This program is free software; you can redistribute it and/or modify
+c it under the terms of the GNU General Public License as published by
+c the Free Software Foundation; either version 3 of the License, or (at
+c your option) any later version.
+c 
+c This program is distributed in the hope that it will be useful, but
+c WITHOUT ANY WARRANTY; without even the implied warranty of
+c MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+c General Public License for more details.
+c 
+c You should have received a copy of the GNU General Public License
+c along with Octave; see the file COPYING.  If not, see
+c <http://www.gnu.org/licenses/>.
+
+      program main
+      end program main
